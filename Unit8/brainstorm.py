3e5e22b1f619a5e3bf363990cd12cb1effc24fc0
JourneyPython <124389842+JourneyPython@users.noreply.github.com>
2025-12-06 11:23:05 -0500

    Add files via upload

---
+++ Unit8/brainstorm.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+    # begin screen
+    # set up interactive background
+    # make a sprite
+    # allow users to interact with the sprite
+    pygame.display.update()
